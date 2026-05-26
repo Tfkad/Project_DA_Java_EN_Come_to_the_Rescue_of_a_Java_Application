745acfc41fd9262febe78e87633ea5a1a8583b73
Tfkad <tfk.kad@gmail.com>
2026-05-26 14:29:46 +0200

    init branche

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,25 +5,28 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
+	private static int headacheCount = 0;	// initialize to 0 
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
+	// err chemin d'accès manquant
+	private static String chemin = "C:\\Users\\C_local\\Documents\\Openclassrooms\\Developpeur_Java\\Projet_2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\\\Project02Eclipse\\";
+		
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader(chemin + "symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int i = 0;	// set i to 0 // err i est inutile
+		// int headacheCount = 0;	// err déjà déclaré
 		while (line != null) {
 			i++;	// increment i
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++; // err headacheCount vs headCount
+				System.out.println("number of headaches: " + headacheCount); // err headacheCount vs headCount
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) { // err rash vs rush
 				rashCount++;
 			}
 			else if (line.contains("pupils")) {
@@ -34,7 +37,7 @@ public class AnalyticsCounter {
 		}
 		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter (chemin + "result.out"); // err ajout chemin
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
